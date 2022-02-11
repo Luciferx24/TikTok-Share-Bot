1332f6de33e4f5a701aed3f981dbaaecae2b31e1
xtekky <98614666+xtekky@users.noreply.github.com>
2022-02-11 21:05:51 +0100

    Create sharebot.py
    Main program

---
+++ sharebot.py
@@ -0,0 +1,82 @@
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import Select
+from time import sleep
+from termcolor import colored
+import sys
+from termcolor import colored, cprint
+from colors import *
+print(color(
+    '''
+                               _____                    _____                    _____                    _____                _____          
+         ______               /\    \                  /\    \                  /\    \                  /\    \              |\    \         
+        |::|   |             /::\    \                /::\    \                /::\____\                /::\____\             |:\____\        
+        |::|   |             \:::\    \              /::::\    \              /:::/    /               /:::/    /             |::|   |        
+        |::|   |              \:::\    \            /::::::\    \            /:::/    /               /:::/    /              |::|   |        
+        |::|   |               \:::\    \          /:::/\:::\    \          /:::/    /               /:::/    /               |::|   |        
+        |::|   |                \:::\    \        /:::/__\:::\    \        /:::/____/               /:::/____/                |::|   |        
+        |::|   |                /::::\    \      /::::\   \:::\    \      /::::\    \              /::::\    \                |::|   |        
+        |::|   |               /::::::\    \    /::::::\   \:::\    \    /::::::\____\________    /::::::\____\________       |::|___|______  
+  ______|::|___|___ ____      /:::/\:::\    \  /:::/\:::\   \:::\    \  /:::/\:::::::::::\    \  /:::/\:::::::::::\    \      /::::::::\    \ 
+ |:::::::::::::::::|    |    /:::/  \:::\____\/:::/__\:::\   \:::\____\/:::/  |:::::::::::\____\/:::/  |:::::::::::\____\    /::::::::::\____|
+ |:::::::::::::::::|____|   /:::/    \::/    /\:::\   \:::\   \::/    /\::/   |::|~~~|~~~~~     \::/   |::|~~~|~~~~~        /:::/~~~~/~~      
+        |::|   |           /:::/    / \/____/  \:::\   \:::\   \/____/  \/____|::|   |           \/____|::|   |            /:::/    /         
+        |::|   |          /:::/    /            \:::\   \:::\    \            |::|   |                 |::|   |           /:::/    /          
+        |::|   |         /:::/    /              \:::\   \:::\____\           |::|   |                 |::|   |          /:::/    /           
+        |::|   |         \::/    /                \:::\   \::/    /           |::|   |                 |::|   |          \::/    /            
+        |::|   |          \/____/                  \:::\   \/____/            |::|   |                 |::|   |           \/____/             
+        |::|   |                                    \:::\    \                |::|   |                 |::|   |                               
+        |::|   |                                     \:::\____\               \::|   |                 \::|   |                               
+        |::|___|                                      \::/    /                \:|   |                  \:|   |                               
+                                                       \/____/                  \|___|                   \|___|                                                                                                                                                              
+'''
+, fg='red'
+))
+
+
+print('creds to: https://github.com/xtekky')
+
+PATH = "C:\Program Files (x86)\chromedriver.exe"
+driver = webdriver.Chrome(PATH)
+driver.get("https://zefoy.com/")
+
+vidUrl = input("url: ")
+
+def loop1():
+    global i
+    sleep(10)
+    try:
+        driver.find_element(By.XPATH, "/html/body/div[4]/div[1]/div[3]/div/div[5]/div/button").click()
+    except:
+        print("Solve Captcha!, Refreshing...")
+        driver.refresh()
+        loop1()
+    try:
+        sleep(0.5)
+        driver.find_element(By.XPATH, "/html/body/div[4]/div[6]/div/form/div/input").send_keys(vidUrl)
+        sleep(1)
+        driver.find_element(By.XPATH, "/html/body/div[4]/div[6]/div/form/div/div/button").click()
+        sleep(1)
+        driver.find_element(By.XPATH, "/html/body/div[4]/div[6]/div/div/div[1]/div/form/button").click()
+        sleep(3)
+        driver.refresh()
+        i += 1
+        total = i * 500
+        print("Success delivered!")
+        sleep(1)
+        loop1()
+    except:
+        print("Timer still running, trying again in a minute")
+        driver.refresh()
+        sleep(5)
+        loop1()
+
+
+bot = int(input("Type 1 to start\n"))
+
+
+if bot == 1:
+    print('starting engine...')
+    loop1()
+else:
+    print('error')
